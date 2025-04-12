700449bef4d09d29c6531d5b4044a050fc90cdcf
Amit Garu <amitgaru2@gmail.com>
2025-04-12 10:35:22 -0600

    Amitgaru2 patch 1 (#8)
    Admin site integration support.

--- example_app/admin.py
+++ example_app/admin.py
@@ -1,3 +1,21 @@
+from django import forms
 from django.contrib import admin
 
+from nepali_datetime_field.forms import NepaliDateField
+
+from example_app.models import ExampleModel
+
+
+class ExampleModelForm(forms.ModelForm):
+    nepali_date = NepaliDateField()
+
+    class Meta:
+        model = ExampleModel
+        fields = '__all__'
+
+
+
 # Register your models here.
+@admin.register(ExampleModel)
+class ExampleModelAdmin(admin.ModelAdmin):
+    form = ExampleModelForm
